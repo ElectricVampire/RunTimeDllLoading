d967cec9b492e7cf3724da051cd973dfbabfd090
Pareek <pareeka2@diebold.com>
2018-01-02 12:54:21 +0530

    First Checkin

---
+++ Process1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Process1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(@"..\..\..\XmlLibrary\bin\Debug\Print.dll");
+            AppDomain.CurrentDomain.Load(assembly.GetName());
+            Type t = assembly.GetType("Print.Class1");
+            dynamic instance = Activator.CreateInstance(t);
+            instance.Display("Process1");
+            Console.ReadLine();
+        }
+    }
+}
